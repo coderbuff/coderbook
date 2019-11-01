b30fa42053c6a92a06e091c73e0758a3fb6a8255
yulinfeng <yulinfeng@nibirutech.com>
2019-11-01 14:19:12 +0800

    comment

---
+++ docs/.vuepress/components/component/component.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'comment',
+  data() {
+    return {};
+  },
+  mounted() {
+    let body = document.querySelector('.gitalk-container');
+    let script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
+    body.appendChild(script);
+    script.onload = () => {
+      const commentConfig = {
+        clientID: 'YOUR_CLINENT_ID',
+        clientSecret: 'YOUR_CLINENT_SECRET',
+        repo: 'coderbook',
+        owner: 'coderbuff',
+        admin: ['coderbuff','yu-linfeng'],
+       
+        id: www.coderbuff.com/coderbook,
+        distractionFreeMode: false,
+      };
+      const gitalk = new Gitalk(commentConfig);
+      gitalk.render('gitalk-container');
+    };
+  },
+};
+</script>
